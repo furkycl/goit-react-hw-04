e31a4f1025d7cac0ec18822e68caf51621b94aff
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-06-16 11:03:39 +0300

    all works fine with css

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,12 +1,21 @@
+import React from "react";
 import styles from "./ImageGallery.module.css";
-import ImageCard from "../ImageCard/ImageCard";
 
 export default function ImageGallery({ images, onImageClick }) {
   return (
     <ul className={styles.gallery}>
       {images.map((image) => (
-        <li key={image.id} className={styles.item}>
-          <ImageCard image={image} onClick={onImageClick} />
+        <li
+          key={image.id}
+          className={styles.galleryItem}
+          onClick={() => onImageClick(image)}
+        >
+          <img
+            src={image.urls.small}
+            alt={image.alt_description || "Image"}
+            className={styles.galleryImage}
+            loading="lazy"
+          />
         </li>
       ))}
     </ul>
